cf27392c7dc1930b3a6a4f194e663ab69378527e
Giebisch <39874595+Giebisch@users.noreply.github.com>
2020-03-31 21:50:44 +0200

    Update

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ countries = []
 dates = []
 
 # Change to Recovered / Deaths / Confirmed
-with open("time_series_19-covid-Confirmed.csv", "r") as csvfile:
+with open("time_series_covid19_deaths_global.csv", "r") as csvfile:
     data = csv.reader(csvfile, delimiter=",")
     dates = [datetime.strptime(date, '%m/%d/%y') for date in next(data)[4:]]
     for country in data:
         # Add countries you want to visualize
-        if country[1] in ["Germany", "Italy", "Spain"]:
+        if country[1] in ["Germany", "Italy", "Spain", "US"]:
             numbers_in_int = [int(number) for number in country[4:]]
             countries.append((country[1], numbers_in_int))
 
@@ -20,4 +20,6 @@ for country in countries:
     plt.plot(dates, country[1], label=country[0])
 
 plt.legend(loc='best')
+plt.xticks(rotation=90)
+plt.grid()
 plt.show()
